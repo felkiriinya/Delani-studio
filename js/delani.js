7fe05e5a9c8413d6ed9046777fd55fd29d5749a1
felkiriinya <felkiriinya@gmail.com>
2020-08-27 00:09:49 +0300

    added and modified the portfolio hover

--- js/delani.js
+++ js/delani.js
@@ -30,3 +30,52 @@ $(document).ready(function () { //hover effect for portfolio images
 		$(".name1").hide();
 	});
 });
+$(document).ready(function () { //hover effect for portfolio images
+	$(".view2").hover(function () {
+		$(".name2").show();
+	}, function () {
+		$(".name2").hide();
+	});
+});
+$(document).ready(function () { //hover effect for portfolio images
+	$(".view3").hover(function () {
+		$(".name3").show();
+	}, function () {
+		$(".name3").hide();
+	});
+});
+$(document).ready(function () { //hover effect for portfolio images
+	$(".view4").hover(function () {
+		$(".name4").show();
+	}, function () {
+		$(".name4").hide();
+	});
+});
+$(document).ready(function () { //hover effect for portfolio images
+	$(".view5").hover(function () {
+		$(".name5").show();
+	}, function () {
+		$(".name5").hide();
+	});
+});
+$(document).ready(function () { //hover effect for portfolio images
+	$(".view6").hover(function () {
+		$(".name6").show();
+	}, function () {
+		$(".name6").hide();
+	});
+});
+$(document).ready(function () { //hover effect for portfolio images
+	$(".view7").hover(function () {
+		$(".name7").show();
+	}, function () {
+		$(".name7").hide();
+	});
+});
+$(document).ready(function () { //hover effect for portfolio images
+	$(".view8").hover(function () {
+		$(".name8").show();
+	}, function () {
+		$(".name8").hide();
+	});
+});
