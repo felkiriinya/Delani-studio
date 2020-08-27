099263449db3fec3239ee959be48a6d83588a2b6
felkiriinya <felkiriinya@gmail.com>
2020-08-27 15:27:42 +0300

    proper indentation, alignment and spacing

--- js/delani.js
+++ js/delani.js
@@ -83,7 +83,6 @@ function sendMessage(){
 		alert("Please fill in all fields!")
 	} else {
 		alert("Thank you " + name + "! Your message has been received. We shall get back to you shortly.");
-        document.getElementById("yourform");
-       
+        document.getElementById("yourform");      
 	};
 };
